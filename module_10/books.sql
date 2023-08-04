b16266b0ed0ac206f316fc5e2232d2454760c51b
main-user <email@email.com>
2023-08-04 17:29:50 -0500

    added sql scripts for WhatABook database

---
+++ module_10/books.sql
@@ -0,0 +1,33 @@
+/*
+Author: Miguel Juan Lorenzo
+
+Script to enter multiple values into book table
+*/
+
+-- INSERT statements for book table --
+INSERT INTO book(book_name, author, details)
+    VALUES('The Return of the King', 'J.R.Tolkien', 'The third part of The Lord of the Rings');
+
+INSERT INTO book(book_name, author, details)
+    VALUES('The Fellowship of the Ring', 'J.R.Tolkien', 'The first part of The Lord of the Rings');
+
+INSERT INTO book(book_name, author, details)
+    VALUES('The Two Towers', 'J.R.Tolkien', "The second part of The Lord of The Rings");
+
+INSERT INTO book(book_name, author)
+    VALUES('The Hobbit or There and Back Again', 'J.R.Tolkien');
+
+INSERT INTO book(book_name, author)
+    VALUES('Dune: Deluxe Edition', 'Frank Herbert');
+
+INSERT INTO book(book_name, author)
+    VALUES("Charlotee's Web", 'E.B. White');
+
+INSERT INTO book(book_name, author)
+    VALUES('The Great Gatsby', 'F. Scott Fitzgerald');
+
+INSERT INTO book(book_name, author)
+    VALUES('The Lion, the Witch, and the Wardrobe', 'C.S. Lewis');
+
+INSERT INTO book(book_name, author)
+    VALUES('The Catcher and the Rye', 'J.D. Salinger');
